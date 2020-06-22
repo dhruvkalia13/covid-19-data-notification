0e89169c2428b1919e3cecb32d280b043e9f8703
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-06-22 16:13:18 +0530

    Minor refactoring

--- covid/covidDataNotification.py
+++ covid/covidDataNotification.py
@@ -5,21 +5,27 @@ import time
 
 from win10toast import ToastNotifier
 
+
 def job():
-    print("Notification incoming (every 15 minutes)...")
+    print("Notification incoming (every hour)...")
     conn = http.client.HTTPSConnection("coronavirus-19-api.herokuapp.com")
     payload = ''
     headers = {}
     conn.request("GET", "/countries/india/", payload, headers)
     res = conn.getresponse()
+
     data = res.read()
-    test1 = json.loads(data)
+    data = json.loads(data)
+    name_of_country = data['country']
+    total_cases = data['cases']
+    total_deaths = data['deaths']
 
     toaster = ToastNotifier()
-    toaster.show_toast(test1['country'], "Total cases - %s \nTotal Deaths - %s" % (test1['cases'], test1['deaths']))
+    toaster.show_toast(name_of_country, "Total cases - %s \nTotal Deaths - %s" % (total_cases, total_deaths))
+
 
 job()
-schedule.every(15).minutes.do(job)
+schedule.every(1).hour.do(job)
 
 while 1:
     schedule.run_pending()
